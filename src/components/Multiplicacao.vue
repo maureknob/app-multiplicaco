dd7533cde73e3fd7fbbdd38116fb929500617f12
maureknob <mauricioknob@gmail.com>
2021-04-23 16:35:24 -0300

    resoluçao das respostas

--- src/components/Multiplicacao.vue
+++ src/components/Multiplicacao.vue
@@ -1,46 +1,60 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">Exercício de multiplicação</v-col>
       <v-col cols="12" class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">Digite o multiplicador e o multiplicando</h1>
       </v-col>
 
-      <v-row justify="center">
-        <v-col cols="4">
-          <v-row align="center">
-            <p class="display-1 mr-5 mb-0">Quanto é </p>
-            <v-text-field class="display-1" v-model.number="multiplicador"></v-text-field>
-          </v-row>
-        </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            <v-row justify="center" class="ma-2">
+              <div class="display-1 mr-5">Quanto é</div>
+              <div class="entrada">
+                <v-text-field
+                  v-mask="'#'"
+                  class="display-1 mt-0 pt-0"
+                  v-model.number="multiplicador"
+                ></v-text-field>
+              </div>
 
-        <v-col cols="4">
-          <v-row align="center">
-            <p class="display-1 mr-5 ml-5 mb-0"> x </p>
-            <v-text-field class="display-1" v-model.number="multiplicando"></v-text-field>
-          </v-row>
-        </v-col>
-
-        <v-col cols="12">
-          <v-card v-if="calculo == true">
-            <v-card-title>Quanto é {{multiplicador}} x {{this.multiplicando}} ?</v-card-title>
-            <v-row no-gutters>
-              <v-col cols="4" v-for="n in multiplicador" :key="n">
-                <v-card width="33,3%" class="ma-5 pa-5 yellow accent-1">
-                  <v-card-title>{{n}}</v-card-title>
-                  <v-card-text class="ma-5 pa-5">
-                    <v-row>
-                      <v-col cols="4" v-for="y in multiplicando" :key="y">
-                        <v-icon class="outlined green lighten-2">{{y}}</v-icon>
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
-                </v-card>
-              </v-col>
+              <p class="display-1 mr-5 ml-5">x</p>
+              <div class="entrada">
+                <v-text-field
+                  v-mask="'#'"
+                  class="display-1 mt-0 pt-0"
+                  v-model.number="multiplicando"
+                ></v-text-field>
+              </div>
             </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
+          </v-card-title>
+          <v-row justify="space-around" no-gutters v-if="calculo == true">
+            <v-col cols="4" v-for="n in multiplicador" :key="n">
+              <v-card width="33,3%" class="ma-5 pa-5 yellow accent-1">
+                <v-card-title>{{n}}</v-card-title>
+                <v-card-text class="ma-5 pa-5">
+                  <v-row>
+                    <v-col cols="4" v-for="y in multiplicando" :key="y">
+                      <v-icon class="outlined green lighten-2">{{y}}</v-icon>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" v-if="calculo == true">
+        <v-card>
+          <v-card-title>Qual a resposta certa?</v-card-title>
+          <v-row justify="center">
+              <v-col cols="3" v-for="r in listaRespostas" :key="r">
+                  <v-btn>{{r}}</v-btn>
+              </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -52,11 +66,30 @@ export default {
   data: () => ({
     multiplicador: null,
     multiplicando: null,
-    contador: 2
+    contador: 2,
+    resposta: 0,
+    listaRespostas: [],
+    opcoes: [],
+    casas: 0
   }),
+  methods: {
+    calcularResposta() {
+      this.resposta = this.multiplicador * this.multiplicando;
+      this.listaRespostas = [
+          Math.floor(Math.random() * 82),
+          Math.floor(Math.random() * 82),
+          Math.floor(Math.random() * 82),
+          Math.floor(Math.random() * 82)
+          ];
+
+      var respostaPos = Math.floor(Math.random() * 4);
+      this.listaRespostas[respostaPos] = this.resposta;
+    }
+  },
   computed: {
     calculo() {
       if (this.multiplicador !== null && this.multiplicando !== null) {
+        this.calcularResposta();
         return true;
       } else {
         return false;
@@ -72,5 +105,9 @@ export default {
   border-radius: 50%;
   height: 56px;
   width: 56px;
+}
+
+.entrada {
+  width: 2vw;
 }
 </style>
